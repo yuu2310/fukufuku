d28efb139aefe77dfe1b1f5d01ee6b66885ab3c7
yuu2310 <fairy.king.van@gmail.com>
2022-10-09 08:20:03 +0000

    seedファイル編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,308 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Admin.create!(
-   email: 'Admin@111',
-   password: '111111',
-)
+
+# Admin.create!(
+#  email: 'Admin@111',
+#  password: '111111',
+# )
+
+# Type.create!(
+#    name: 'トップス'
+# )
+
+# Type.create!(
+#    name: 'ジャケット/アウター'
+# )
+
+# Type.create!(
+#    name: 'パンツ'
+# )
+
+# Type.create!(
+#    name: 'シューズ'
+# )
+
+# Type.create!(
+#    name: 'アクセサリー'
+# )
+
+# Type.create!(
+#    name: 'アクセサリー'
+# )
+
+# Category.create!(
+#    category_name: 'Tシャツ/カットソー',
+#    type_id: 1
+# )
+
+# Category.create!(
+#    category_name: 'シャツ/ブラウス',
+#    type_id: 1
+# )
+
+# Category.create!(
+#    category_name: 'ポロシャツ',
+#    type_id: 1
+# )
+
+# Category.create!(
+#    category_name: 'ニット/セーター',
+#    type_id: 1
+# )
+
+# Category.create!(
+#    category_name: 'ベスト',
+#    type_id: 1
+# )
+
+# Category.create!(
+#    category_name: 'パーカー',
+#    type_id: 1
+# )
+
+# Category.create!(
+#    category_name: 'スウェット',
+#    type_id: 1
+# )
+
+# Category.create!(
+#    category_name: 'カーディガン/ボレロ',
+#    type_id: 1
+# )
+
+# Category.create!(
+#    category_name: 'ワンピース',
+#    type_id: 1
+# )
+
+# Category.create!(
+#    category_name: 'その他トップス',
+#    type_id: 1
+# )
+
+# Category.create!(
+#    category_name: 'テーラードジャケット',
+#    type_id: 2
+# )
+
+# Category.create!(
+#    category_name: 'ノーカラージャケット',
+#    type_id: 2
+# )
+
+# Category.create!(
+#    category_name: 'ノーカラーコート',
+#    type_id: 2
+# )
+
+# Category.create!(
+#    category_name: 'デニムジャケット',
+#    type_id: 2
+# )
+
+# Category.create!(
+#    category_name: 'ブルゾン',
+#    type_id: 2
+# )
+
+# Category.create!(
+#    category_name: 'ミリタリージャケット',
+#    type_id: 2
+# )
+
+# Category.create!(
+#    category_name: 'ダウンジャケット/ コート',
+#    type_id: 2
+# )
+
+# Category.create!(
+#    category_name: 'ダッフルコート',
+#    type_id: 2
+# )
+
+# Category.create!(
+#    category_name: 'モッズコート',
+#    type_id: 2
+# )
+
+# Category.create!(
+#    category_name: 'ピーコート',
+#    type_id: 2
+# )
+
+# Category.create!(
+#    category_name: 'ステンカラーコート',
+#    type_id: 2
+# )
+
+# Category.create!(
+#    category_name: 'トレンチコート',
+#    type_id: 2
+# )
+
+# Category.create!(
+#    category_name: 'チェスターコート',
+#    type_id: 2
+# )
+
+# Category.create!(
+#    category_name: 'セットアップ',
+#    type_id: 2
+# )
+
+# Category.create!(
+#    category_name: '--',
+#    type_id: 2
+# )
+
+# Category.create!(
+#    category_name: 'その他アウター',
+#    type_id: 2
+# )
+
+# Category.create!(
+#    category_name: 'デニムパンツ',
+#    type_id: 3
+# )
+
+# Category.create!(
+#    category_name: 'カーゴパンツ',
+#    type_id: 3
+# )
+
+
+# Category.create!(
+#    category_name: 'チノパンツ',
+#    type_id: 3
+# )
+
+
+# Category.create!(
+#    category_name: 'スラックス',
+#    type_id: 3
+# )
+
+
+# Category.create!(
+#    category_name: 'その他パンツ',
+#    type_id: 3
+# )
+
+
+# Category.create!(
+#    category_name: 'スニーカー',
+#    type_id: 4
+# )
+
+# Category.create!(
+#    category_name: 'スリッポン',
+#    type_id: 4
+# )
+
+# Category.create!(
+#    category_name: 'サンダル',
+#    type_id: 4
+# )
+
+# Category.create!(
+#    category_name: 'ブーツ',
+#    type_id: 4
+# )
+
+# Category.create!(
+#    category_name: 'ドレスシューズ',
+#    type_id: 4
+# )
+
+# Category.create!(
+#    category_name: 'ローファー',
+#    type_id: 4
+# )
+
+# Category.create!(
+#    category_name: 'その他シューズ',
+#    type_id: 4
+# )
+
+# Category.create!(
+#    category_name: 'ネックレス',
+#    type_id: 5
+# )
+
+# Category.create!(
+#    category_name: 'リング',
+#    type_id: 5
+# )
+
+# Category.create!(
+#    category_name: 'ピアス(両耳用)',
+#    type_id: 5
+# )
+
+# Category.create!(
+#    category_name: 'ピアス(片耳用)',
+#    type_id: 5
+# )
+
+# Category.create!(
+#    category_name: 'イヤリング',
+#    type_id: 5
+# )
+
+# Category.create!(
+#    category_name: 'イヤーカフ',
+#    type_id: 5
+# )
+
+# Category.create!(
+#    category_name: 'ブレスレット',
+#    type_id: 5
+# )
+
+# Category.create!(
+#    category_name: '--',
+#    type_id: 5
+# )
+
+# Category.create!(
+#    category_name: 'ネックレス',
+#    type_id: 6
+# )
+
+# Category.create!(
+#    category_name: 'リング',
+#    type_id: 6
+# )
+
+# Category.create!(
+#    category_name: 'ピアス(両耳用)',
+#    type_id: 6
+# )
+
+# Category.create!(
+#    category_name: 'ピアス(片耳用)',
+#    type_id: 6
+# )
+
+# Category.create!(
+#    category_name: 'イヤリング',
+#    type_id: 6
+# )
+
+# Category.create!(
+#    category_name: 'イヤーカフ',
+#    type_id: 6
+# )
+
+# Category.create!(
+#    category_name: 'ブレスレット',
+#    type_id: 6
+# )
+
+# Category.create!(
+#    category_name: '--',
+#    type_id: 6
+# )
+
+
